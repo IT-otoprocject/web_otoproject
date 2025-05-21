4adf2bdb1e3927423f43d131127b2563cba0eeb2
tom <daffautomo54@gmail.com>
2025-05-21 09:53:40 +0700

    testing

--- resources/views/spk/create.blade.php
+++ resources/views/spk/create.blade.php
@@ -3,7 +3,6 @@
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('SPK') }}
         </h2>
-        <!-- testing -->
     </x-slot>
 
     <div class="py-12">
